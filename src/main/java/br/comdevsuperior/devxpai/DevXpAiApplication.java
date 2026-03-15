d2890849cdf49101748e145f0a1a1f9a6ff7bb78
rogersilva <rogersilva0308@gmail.com>
2026-03-15 00:32:10 -0300

    Commit inicial

---
+++ src/main/java/br/comdevsuperior/devxpai/DevXpAiApplication.java
@@ -0,0 +1,13 @@
+package br.comdevsuperior.devxpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevXpAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevXpAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/comdevsuperior/devxpai/DevXpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.comdevsuperior.devxpai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevXpAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
